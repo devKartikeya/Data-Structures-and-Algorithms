169b5ef008cb128de7511e728853de14bb430c66
devKartikeya <kartikeya2122008@gmail.com>
2026-03-15 13:15:01 +0530

    Add Selection Sort implementation
    Implement Selection Sort algorithm in JavaScript.

---
+++ Algorithms/Selection_Sort.js
@@ -0,0 +1,28 @@
+/*WAP to implement Selection Sort*/
+
+function selectionSort(arr) {
+    let n = arr.length;
+
+    for (let i = 0; i < n - 1; i++) {
+
+        let minIndex = i;
+
+        for (let j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // Swap
+        if (minIndex !== i) {
+            let temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    return arr;
+}
+
+let arr = [64, 25, 12, 22, 11];
+console.log(selectionSort(arr));
