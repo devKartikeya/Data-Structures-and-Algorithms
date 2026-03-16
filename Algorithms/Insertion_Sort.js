d6eb6a0306eaaef5de7e8eb64ea3771e37c67f1e
devKartikeya <kartikeya2122008@gmail.com>
2026-03-16 20:52:04 +0530

    Add Insertion Sort implementation
    Implement Insertion Sort algorithm in JavaScript.

---
+++ Algorithms/Insertion_Sort.js
@@ -0,0 +1,25 @@
+/* Insertion Sort */
+
+function insertionSort(arr) {
+    let n = arr.length;
+
+    for (let i = 1; i < n; i++) {
+
+        let key = arr[i];      
+        
+        let j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+
+    return arr;
+}
+
+// Example
+let arr = [8, 3, 5, 2, 4];
+console.log(insertionSort(arr));
