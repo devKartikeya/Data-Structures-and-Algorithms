8e4a221be532c101f270605f0d3a589e7d1660e7
Kartikeya Mishra <kartikeya2122008@gmail.com>
2026-06-28 10:37:28 +0530

    Add Patterns

---
+++ Patterns/Pattern_01.js
@@ -0,0 +1,12 @@
+/* Print Rectangle */
+
+function printRectangle() {
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            process.stdout.write("* ");
+        }
+        console.log("\n");
+    }
+}
+
+printRectangle();
