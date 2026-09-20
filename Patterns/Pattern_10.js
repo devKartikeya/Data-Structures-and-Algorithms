ef4e88406d3d230b40a26f9c515dbac96fad94b6
Kartikeya Mishra <kartikeya2122008@gmail.com>
2026-09-21 00:22:33 +0530

    Add more number patterns

---
+++ Patterns/Pattern_10.js
@@ -0,0 +1,18 @@
+/* Print natural number sequentially */
+
+/*
+
+1
+2 3
+4 5 6 
+7 8 9 10
+11 12 13 14 
+
+*/
+
+for (let i = 1; i <= 5; i++){
+    for (let j = 1; j <= i; j++){
+        process.stdout.write((j + (i * (i - 1)) / 2).toString() + " ");
+    }
+    process.stdout.write("\n");
+}
